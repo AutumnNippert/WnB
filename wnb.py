1b1fff73dd7fe7366b1fbe6fd3b42fe9baac4613
Chris Nippert <chrisnippert22@gmail.com>
2023-07-20 22:39:36 -0400

    made better

--- wnb.py
+++ wnb.py
@@ -8,19 +8,32 @@ from watchdog.events import FileSystemEventHandler
 
 print("Watch and Backup! - Created by Cardigan")
 
+application_path = ""
+# determine if application is a script file or frozen exe (pyinstaller shenanegans)
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
+
+if application_path == "":
+    print("Unable to determine application path!")
+    sys.exit(1)
+
 if len(sys.argv) != 2:
-    print("Usage: wnb.exe <folder_name>")
+    print("Usage: ./wnb.exe <folder_name>")
     print("If using python, run 'python wnb.py <folder_name>'")
     sys.exit(1)
 else:
     path = sys.argv[1]
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    backup_path = os.path.join(script_dir, "backup")
-    log_path = os.path.join(script_dir, "log")
+    script_dir = application_path
+    backup_path = os.path.join(script_dir, "wnb_backups")
+    log_path = os.path.join(script_dir, "wnb_logs")
+
+print(f"Locations: {path}, {backup_path}, {log_path}")
 
 #check path exists
 if not os.path.exists(path):
-    print(f"Folder ./{path} does not exist!")
+    print(f"Folder '{path}' does not exist!")
     sys.exit(1)
 
 # Ensure the log folder exists
@@ -29,10 +42,15 @@ os.makedirs(log_path, exist_ok=True)
 
 # Log file path
 log_filename = os.path.join(log_path, "change_log.txt")
+error_filename = os.path.join(log_path, "error_log.txt")
 
 def write_to_change_log(event):
     with open(log_filename, "a") as log_file:
         log_file.write(f"{datetime.now()}: {event}\n")
+
+def write_to_error_log(event):
+    with open(error_filename, "a") as error_file:
+        error_file.write(f"{datetime.now()}: {event}\n")
 
 # Function to perform the initial full backup of the folder
 def backup(src_folder):
@@ -42,15 +60,25 @@ def backup(src_folder):
     dst_folder = os.path.join(backup_path, dt_string)
     for root, dirs, files in os.walk(src_folder):
         for file in files:
-            src_file = os.path.join(root, file)
-            dst_file = os.path.join(dst_folder, os.path.relpath(src_file, src_folder))
-            os.makedirs(os.path.dirname(dst_file), exist_ok=True)
-            shutil.copy2(src_file, dst_file)
+            src_file = ""
+            try:
+                src_file = os.path.join(root, file)
+                dst_file = os.path.join(dst_folder, os.path.relpath(src_file, src_folder))
+                os.makedirs(os.path.dirname(dst_file), exist_ok=True)
+                shutil.copy2(src_file, dst_file)
+            except Exception as e:
+                print(f"Error backing up {src_file}: {str(e)}")
+                write_to_error_log(f"Error backing up {src_file}: {str(e)}")
 
         for dir in dirs:
-            src_dir = os.path.join(root, dir)
-            dst_dir = os.path.join(dst_folder, os.path.relpath(src_dir, src_folder))
-            os.makedirs(dst_dir, exist_ok=True)
+            src_dir = ""
+            try:
+                src_dir = os.path.join(root, dir)
+                dst_dir = os.path.join(dst_folder, os.path.relpath(src_dir, src_folder))
+                os.makedirs(dst_dir, exist_ok=True)
+            except Exception as e:
+                print(f"Error backing up {src_dir}: {str(e)}")
+                write_to_error_log(f"Error backing up {src_dir}: {str(e)}")
     
 # Perform the initial full backup
 backup(path)
@@ -82,7 +110,7 @@ observer.schedule(event_handler, path=path, recursive=True)
 observer.start()
 
 try:
-    print(f"Watching for changes in ./{path}")
+    print(f"Watching for changes in {path}")
     # Run the observer indefinitely in a loop
     while True:
         time.sleep(1)
